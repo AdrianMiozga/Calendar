b7cab7926883e4f9e0319f5761905c5c5335cd0a
Adrian Miozga <AdrianMiozga@outlook.com>
2024-07-15 10:40:11 +0200

    Clean up project

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,4 @@ dependencies {
     val retrofit = "2.11.0"
     implementation("com.squareup.retrofit2:retrofit:$retrofit")
     implementation("com.squareup.retrofit2:converter-gson:$retrofit")
-
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
