ed7d5666731970675f2424e58f5721d7edf65799
Adrian Miozga <AdrianMiozga@outlook.com>
2024-07-15 10:10:20 +0200

    Calculate time spent in all events

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
 import org.example.data.CalendarRepository;
+import org.example.data.Event;
 import org.example.data.EventResponse;
 import retrofit2.Response;
+
+import java.time.Duration;
+import java.time.OffsetDateTime;
 
 
 public class Main {
@@ -11,7 +15,28 @@ public class Main {
         CalendarRepository calendarRepository = new CalendarRepository();
 
         Response<EventResponse> response = calendarRepository.getEvents();
-        System.out.println(response.code());
-        System.out.println(response.body());
+        EventResponse eventResponse = response.body();
+
+        if (eventResponse == null) {
+            return;
+        }
+
+        long totalSeconds = 0;
+
+        for (Event event : eventResponse.items()) {
+            OffsetDateTime start = OffsetDateTime.parse(event.start().dateTime());
+            OffsetDateTime end = OffsetDateTime.parse(event.end().dateTime());
+
+            Duration duration = Duration.between(start, end);
+
+            totalSeconds += duration.getSeconds();
+        }
+
+        long hours = totalSeconds / 3600;
+        long minutes = (totalSeconds % 3600) / 60;
+        long seconds = totalSeconds % 60;
+
+        String formattedDuration = String.format("%dh %dm %ds", hours, minutes, seconds);
+        System.out.println("Total for all events: " + formattedDuration);
     }
 }
--- src/main/java/org/example/data/EventResponse.java
+++ src/main/java/org/example/data/EventResponse.java
@@ -3,6 +3,7 @@ package org.example.data;
 import java.util.List;
 
 
+// TODO: rename items to events
 public record EventResponse(List<Event> items) {
 
 }
