abc8bcbb986f9ba7090725a10508ed7d21ef58e3
Adrian Miozga <AdrianMiozga@outlook.com>
2024-07-16 09:36:06 +0200

    Add main entry class to jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,14 @@ repositories {
     mavenCentral()
 }
 
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "org.example.Main"
+        )
+    }
+}
+
 dependencies {
     val retrofit = "2.11.0"
     implementation("com.squareup.retrofit2:retrofit:$retrofit")
