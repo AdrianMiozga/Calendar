22e0eabedb235eedc1557a54d71ff175890f6a4b
Adrian Miozga <AdrianMiozga@outlook.com>
2024-07-29 13:43:58 +0200

    Show error when no config file provided

--- src/main/java/org/wentura/calendar/data/config/ConfigRepository.java
+++ src/main/java/org/wentura/calendar/data/config/ConfigRepository.java
@@ -4,7 +4,9 @@ import com.google.gson.Gson;
 
 import org.wentura.calendar.config.Constants;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 
 public class ConfigRepository {
@@ -20,7 +22,11 @@ public class ConfigRepository {
             var gson = new Gson();
             config = gson.fromJson(reader, Config.class);
             return config;
-        } catch (Exception exception) {
+        } catch (NoSuchFileException exception) {
+            System.out.println("No configuration file (" + Constants.CONFIG_FILENAME + ")");
+            System.exit(1);
+            return null;
+        } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
     }
